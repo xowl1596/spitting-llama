b2f5a77fd2bfc31e11a689b96cd6c5493fb7faba
DESKTOP-GCEC1A4\JTJ <xowl1596@gmail.com>
2022-01-04 11:01:17 +0900

    불필요한 코드 제거

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
