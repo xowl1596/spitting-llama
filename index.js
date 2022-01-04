95c019124fce2f645c7dffffbeef72f2bb56a6da
DESKTOP-GCEC1A4\JTJ <xowl1596@gmail.com>
2022-01-04 10:55:40 +0900

    ????

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,4 +8,4 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 });
 
-server.listen(port || 5000);
+server.listen(process.env.PORT || 5000);
