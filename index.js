4568e9bb71b7ef03455d63fb87299d6fc53a2444
DESKTOP-GCEC1A4\JTJ <xowl1596@gmail.com>
2022-01-04 11:03:00 +0900

    asas

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(process.env.PORT || 5000);
