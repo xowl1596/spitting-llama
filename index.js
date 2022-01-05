d22adb6a9d9ba81f377f9be179f8079a5370bc61
DESKTOP-GCEC1A4\JTJ <xowl1596@gmail.com>
2022-01-05 17:58:35 +0900

    욕필터 임시 제거

--- index.js
+++ index.js
@@ -67,12 +67,12 @@ client.on('messageCreate', message => {
       message.channel.send({ files: [{ attachment: './llama2.png' }] });
     }
     
-    for(let i = 0 ; i < fwordList.length; i++){
-      if(message.content.indexOf(fwordList[i]) !== -1){
-        message.channel.send('욕하지 마라! 퉤엣!');
-        break;
-      }
-    }
+    // for(let i = 0 ; i < fwordList.length; i++){
+    //   if(message.content.indexOf(fwordList[i]) !== -1){
+    //     message.channel.send('욕하지 마라! 퉤엣!');
+    //     break;
+    //   }
+    // }
 });
 client.login(process.env.TOKEN);
 
