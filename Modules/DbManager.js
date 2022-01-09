bcee017b5e69f3859befac920863607f82aabb1f
DESKTOP-87EM2QL\TaejiJang <xowl1596@gmail.com>
2022-01-09 15:23:48 +0900

    콘솔 출력 제거

--- Modules/DbManager.js
+++ Modules/DbManager.js
@@ -188,7 +188,7 @@ module.exports = class DbManager{
 
     async checkGuild(guildId){
         let guild = await this.getGuildById(guildId);
-        console.log(guild);
+
         if(typeof guild == 'undefined') { 
             return 'NO_REGIST'; 
         }
